fe1c064a964d87743a6e6e6eb9c36bd75ee624b2
JimlvRong <chinargb@chinargbdeMac-mini.local>
2016-12-22 17:22:05 +0800

    create.

---
+++ someTools/someTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  someTools
+//
+//  Created by chinargb on 2016/12/22.
+//  Copyright © 2016年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ someTools/someTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  someTools
+//
+//  Created by chinargb on 2016/12/22.
+//  Copyright © 2016年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
